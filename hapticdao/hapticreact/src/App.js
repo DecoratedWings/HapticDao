cd95091dd724a9e8c258d2900e58343faa5b22d1
Mona <tigerzmona@yahoo.com>
2022-05-07 20:25:49 -0400

    setup project to use TailWind

--- hapticdao/hapticreact/src/App.js
+++ hapticdao/hapticreact/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          UI in progress.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
       </header>
     </div>
   );
