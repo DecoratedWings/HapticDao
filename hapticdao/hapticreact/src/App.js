980c725c32fb23931ea4c557716c4e50ce387cb1
Mona <tigerzmona@yahoo.com>
2022-05-07 21:46:31 -0400

    embedded iframe, need to adjust

--- hapticdao/hapticreact/src/App.js
+++ hapticdao/hapticreact/src/App.js
@@ -13,6 +13,9 @@ function App() {
           UI IN PROGRESS.
         </p>
       </header> */}
+
+        {/* <iframe src='https://my.spline.design/hapticlogo-fb22e1ef24b65fc8f1b9fe2b5b3f7010/' frameborder='0' width='100%' height='100%'></iframe> */}
+        <iframe src='https://my.spline.design/hapticlogo-fb22e1ef24b65fc8f1b9fe2b5b3f7010/' frameborder='100' width='100%' height='100%'></iframe>      
     </div>
   );
 }
