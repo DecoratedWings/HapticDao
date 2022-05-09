a8379bb32361aed3b955c28d6cc4a2168b33b7a8
Mona <tigerzmona@yahoo.com>
2022-05-09 19:41:25 -0400

    mainpage fully functioning with voice playback

--- hapticdao/hapticreact/src/components/MainPage.jsx
+++ hapticdao/hapticreact/src/components/MainPage.jsx
@@ -1,8 +1,48 @@
-import {React, Link} from 'react'
+import React from 'react'
 import { FaArrowsAltH, FaBtc, FaStore, FaThList } from 'react-icons/fa';
 import Card from './Card';
+import { useSpeechSynthesis } from 'react-speech-kit';
+import HapticVibrationService from '../services/HapticVibrationService';
+
 
 const MainPage = () => {
+
+  const hapticVibrationService = new HapticVibrationService;
+  const { speak } = useSpeechSynthesis();
+
+  async function selectSend() {
+    var status = await hapticVibrationService.selectionVibrate(function (fallback) {
+        console.log("Vibration encountered an error: ", fallback);
+    });
+    speak({ text: "Send" });
+}
+async function selectDao() {
+    var status = await hapticVibrationService.selectionVibrate(function (fallback) {
+        console.log("Vibration encountered an error: ", fallback);
+    });
+    //DAO pronunciation lol
+    speak({ text: "dow" });
+}
+async function selectSwap() {
+    var status = await hapticVibrationService.selectionVibrate(function (fallback) {
+        console.log("Vibration encountered an error: ", fallback);
+    });
+    speak({ text: "Swap" });
+}
+async function selectMarketplace() {
+    var status = await hapticVibrationService.selectionVibrate(function (fallback) {
+        console.log("Vibration encountered an error: ", fallback);
+    });
+    speak({ text: "Marketplace" });
+}
+
+async function selectDashboard() {
+    var status = await hapticVibrationService.selectionVibrate(function (fallback) {
+        console.log("Vibration encountered an error: ", fallback);
+    });
+    speak({ text: "Dashboard" });
+}
+
   return (
 
     <div name='dao' className='w-full h-screen justify-center bg-teal-100'>
@@ -18,10 +58,10 @@ const MainPage = () => {
         <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
 
           {/* Card */}
-          <Card icon={<FaThList size={40} />} heading='Haptic Dashboard' text='Dashboard For viewing your tokens and NFTs' page='dashboard'/>
-          <Card icon={<FaBtc size={40} />} heading='Haptic Send' text='Transfer from any valid chain that our application supports to recipients addresses on the respective chain.' page='swap'/>
-          <Card icon={<FaArrowsAltH size={40} />} heading='Haptic DEX' text='Decentralized exchange that empowers users to trade their crypto-currencies.'page='send' />
-          <Card icon={<FaStore size={40} />} heading='Haptic Market' text='NFT Marketplace that meets the needs of the community. Buy/sell NFTs on the marketplace for 0 fees and select your own royalty policies.'page='marketplace' />
+          <Card icon={<FaThList size={40} />} heading='Haptic Dashboard' text='Dashboard For viewing your tokens and NFTs' page='dashboard' choice={selectDashboard}/>
+          <Card icon={<FaBtc size={40} />} heading='Haptic Send' text='Transfer from any valid chain that our application supports to recipients addresses on the respective chain.' page='send' choice={selectSend}/>
+          <Card icon={<FaArrowsAltH size={40} />} heading='Haptic DEX' text='Decentralized exchange that empowers users to trade their crypto-currencies.'page='swap' choice={selectSwap}/>
+          <Card icon={<FaStore size={40} />} heading='Haptic Market' text='NFT Marketplace that meets the needs of the community. Buy/sell NFTs on the marketplace for 0 fees and select your own royalty policies.'page='marketplace' choice={selectMarketplace}/>
         </div>
       </div>
 
