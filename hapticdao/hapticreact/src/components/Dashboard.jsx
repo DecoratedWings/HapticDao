7f9fc3d2e2b37bfc09a1e93640846173896bccbc
Mona <tigerzmona@yahoo.com>
2022-05-10 13:26:32 -0400

    detects token from voice correctly

--- hapticdao/hapticreact/src/components/Dashboard.jsx
+++ hapticdao/hapticreact/src/components/Dashboard.jsx
@@ -28,7 +28,10 @@ const Dashboard = () => {
         setIsListening(true);
         SpeechRecognition.startListening();
         console.log("transcript is: ",transcript);
-        // return transcript;
+
+        await hapticVibrationService.successVibrate(function (fallback) {
+            console.log("Vibration encountered an error: ", fallback);
+        });
     }
 
     return (
@@ -50,16 +53,23 @@ const Dashboard = () => {
             <div className='absolute flex flex-col py-8 md:min-w-[760px] 
             mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-gray-500
             border border-slate-300 rounded-xl text-center shadow-xl'>
-                <h2 className='text-slate-300'><b>What Crypto would you like the price of?</b> </h2>
+                <h2 className='text-slate-300 text-2xl'><b>What Crypto would you like the price of?</b> </h2>
                 <br/>
                 <div className='max-w-[1000px] mx-auto px-20  justify-center '>
                 <button class="bg-teal-500 hover:bg-teal-700 text-white font-bold px-10 py-2 rounded-full" onClick={getTokenData}>
                    Speak
                 </button>
+                <div className='flex justify-between flex-wrap px-4'>
+                    <p className='flex px-4 py-2 text-slate-300'><FaBtc className='h-6 text-green-200'/>&nbsp;Token Selected :</p>
+                    <br/>
+                   
                 </div>
                 <div className='flex justify-between flex-wrap px-4'>
-                    <p className='flex px-4 py-2 text-slate-300'><FaBtc className='h-6 text-green-200' /> Coin</p>
+                    <p className='flex px-10 py-2 text-teal-300 text-2xl'>{transcript}</p>
                 </div>
+
+                </div>
+           
             </div>
 
         </div>
