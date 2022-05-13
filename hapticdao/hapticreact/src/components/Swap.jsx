40ee44017447ce22c54700ac3082524c7b23e8ee
Mona <tigerzmona@yahoo.com>
2022-05-13 18:52:59 -0400

    sort've works, taking a break for now

--- hapticdao/hapticreact/src/components/Swap.jsx
+++ hapticdao/hapticreact/src/components/Swap.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useMoralis, useOneInchSwap } from "react-moralis";
+
 
 const Swap = () => {
+    const { Moralis, isInitialized } = useMoralis();
+
+    useEffect(() => {
+        if(isInitialized){
+         // your code
+        }
+      }, [isInitialized])
+
+    async function init() {
+            await Moralis.initPlugins();
+            await Moralis.enableWeb3();
+            const tokens = await Moralis.Plugins.oneInch.getSupportedTokens({
+              chain: 'eth', // The blockchain you want to use (eth/bsc/polygon)
+            });
+            console.log(tokens);
+    }
+
+
     return (
         <div name='dao' className='w-full h-screen justify-center bg-teal-100'>
             <div className='max-w-[300px] mx-auto px-8  justify-center '>
                 <br /><br /><br /> <br />
                 <h1 className='text-4xl sm:text-7xl font-bold items-center justify-center text-gray-500'>Haptic DEX</h1>
                 <br /><br /><br /> <br />
-            </div>
 
+                <button onClick={init}>CLICK ME</button>
+            </div>
         </div>
     )
 }
