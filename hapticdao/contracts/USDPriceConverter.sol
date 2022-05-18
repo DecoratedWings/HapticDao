fdc8097d863f534723edc4e929408d8a7f32ed11
Dan Rassouli <Drassouli14@gmail.com>
2022-05-18 17:56:24 -0400

    Updated Aggregator Variables

--- hapticdao/contracts/USDPriceConverter.sol
+++ hapticdao/contracts/USDPriceConverter.sol
@@ -5,12 +5,23 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract USDPriceConverter {
 
- AggregatorV3Interface internal priceFeed;
+ AggregatorV3Interface internal priceFeedEthUSD;
+ AggregatorV3Interface internal priceFeedIlvUSD;
+ AggregatorV3Interface internal priceFeedBtcUSD;
+ AggregatorV3Interface internal priceFeedLinkUSD;
+ AggregatorV3Interface internal priceFeedLtcUSD;
+ AggregatorV3Interface internal priceFeedRepUSD;
+ AggregatorV3Interface internal priceFeedSnxUSD;
+ AggregatorV3Interface internal priceFeedXauUSD;
+ AggregatorV3Interface internal priceFeedTrxUSD;
+ AggregatorV3Interface internal priceFeedXrpUSD;
+ AggregatorV3Interface internal priceFeedZrxUSD;
+ AggregatorV3Interface internal priceFeedXagUSD;
+ AggregatorV3Interface internal priceFeedXtzUSD;
 
     /**
      * Network: Rinkeby
-     * Aggregator: ETH/USD
-     * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+     * Aggregator: Eth, Ilv, Btc, Link, Ltc, Rep, Snx, Trx, Xau, Xrp, Zrx, Xag, Xtz / USD
      */
     constructor() {
         priceFeedEthUSD = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
@@ -24,7 +35,7 @@ contract USDPriceConverter {
         priceFeedTrxUSD = AggregatorV3Interface(0xb29f616a0d54FF292e997922fFf46012a63E2FAe);
         priceFeedXrpUSD = AggregatorV3Interface(0xc3E76f41CAbA4aB38F00c7255d4df663DA02A024);
         priceFeedZrxUSD = AggregatorV3Interface(0xF7Bbe4D7d13d600127B6Aa132f1dCea301e9c8Fc);
-        priceFeedXAGUSD = AggregatorV3Interface(0x9c1946428f4f159dB4889aA6B218833f467e1BfD);
+        priceFeedXagUSD = AggregatorV3Interface(0x9c1946428f4f159dB4889aA6B218833f467e1BfD);
         priceFeedXtzUSD = AggregatorV3Interface(0xf57FCa8B932c43dFe560d3274262b2597BCD2e5A);
     }
 
