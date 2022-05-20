134e3202d206447496f0975545e997498d3e2bdc
Mona <tigerzmona@yahoo.com>
2022-05-19 20:27:00 -0400

    utilizing iframe in lieu of widget issues :(

--- hapticdao/hapticreact/src/components/Swap.jsx
+++ hapticdao/hapticreact/src/components/Swap.jsx
@@ -1,47 +1,27 @@
 import React, { useEffect } from 'react'
-import { useMoralis, useOneInchSwap } from "react-moralis";
+import { useMoralis } from "react-moralis";
 
 
 const Swap = () => {
-    const { Moralis, isInitialized } = useMoralis();
-
-    useEffect(() => {
-        if (isInitialized) {
-            // your code
-        }
-    }, [isInitialized])
-
-    async function init() {
-        await Moralis.initPlugins();
-        await Moralis.enableWeb3();
-
-        await getAvailableTokens();
-    }
-
-    async function getAvailableTokens(){
-        const result = await Moralis.Plugins.oneInch.getSupportedTokens({
-            chain: 'eth', // The blockchain you want to use (eth/bsc/polygon)
-        });
-
-        const tokens = result.tokens;
-        console.log(tokens);
-
-        for(const address in tokens){
-            let token = tokens[address];
-            
-        }
-    }
-
 
     return (
-        <div name='dao' className='w-full h-screen justify-center bg-teal-100'>
+        <div name='dex' className='w-full h-screen justify-center bg-teal-100'>
             <div className='max- w-[300px] mx-auto px-8  justify-center '>
                 <br /><br /><br /> <br />
                 <h1 className='text-4xl sm:text-7xl font-bold items-center justify-center text-gray-500'>Haptic DEX</h1>
                 <br /><br /><br /> <br />
 
-                <button onClick={init}>CLICK ME</button>
+                {/* <button onClick={init}>CLICK ME</button> */}
             </div>
+
+    
+            <iframe src="https://app.uniswap.org/#/swap?exactField=input&exactAmount=10&inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f"
+                height="660px"
+                width="100%"
+                style={{border: 0, margin: '0 auto', marginBottom: '0.5rem', 
+                display:'block', borderRadius:'10px', maxWidth:'960px', minWidth:'300px'}}
+            />
+          
         </div>
     )
 }
