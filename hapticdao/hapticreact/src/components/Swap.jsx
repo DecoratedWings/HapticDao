47aa31bfddeffb4c289501ff697005513b995436
Mona <tigerzmona@yahoo.com>
2022-05-19 20:28:19 -0400

    code cleanup

--- hapticdao/hapticreact/src/components/Swap.jsx
+++ hapticdao/hapticreact/src/components/Swap.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useMoralis } from "react-moralis";
+import React from 'react'
 
 
 const Swap = () => {
@@ -9,18 +8,17 @@ const Swap = () => {
             <div className='max- w-[300px] mx-auto px-8  justify-center '>
                 <br /><br /><br /> <br />
                 <h1 className='text-4xl sm:text-7xl font-bold items-center justify-center text-gray-500'>Haptic DEX</h1>
-                <br /><br /><br /> <br />
-
-                {/* <button onClick={init}>CLICK ME</button> */}
+               
             </div>
-
-    
             <iframe src="https://app.uniswap.org/#/swap?exactField=input&exactAmount=10&inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f"
                 height="660px"
                 width="100%"
                 style={{border: 0, margin: '0 auto', marginBottom: '0.5rem', 
                 display:'block', borderRadius:'10px', maxWidth:'960px', minWidth:'300px'}}
             />
+
+
+     
           
         </div>
     )
